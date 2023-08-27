ae15cc1fde9dd5cd8ecf8cbda3dac46a4090ca06
Robot <zhangwanjie@126.com>
2023-08-27 21:11:23 +0800

    update slam.launch

--- launch/slam.launch.py
+++ launch/slam.launch.py
@@ -21,7 +21,6 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
