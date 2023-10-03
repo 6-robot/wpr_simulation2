7785774460a526a578a5056aef27d784a8c4f48b
Robot <zhangwanjie@126.com>
2023-10-03 19:09:28 +0800

    update demo_3_launch

--- demo_launch/3_pub_sub.launch.py
+++ demo_launch/3_pub_sub.launch.py
@@ -2,19 +2,22 @@ from launch_ros.actions import Node
 from launch import LaunchDescription
 
 def generate_launch_description():
+
     publisher_cmd = Node(
-            package='wpr_simulation2',
-            executable='3_publisher_node',
-            name='publisher_node'
+        package='topic_pkg',
+        executable='publisher_node',
+        name='publisher_node'
         )
-    
+
     subscriber_cmd = Node(
-            package='wpr_simulation2',
-            executable='3_subscriber_node',
-            name='subscriber_node'
+        package='topic_pkg',
+        executable='subscriber_node',
+        name='subscriber_node'
         )
 
     ld = LaunchDescription()
+
     ld.add_action(publisher_cmd)
     ld.add_action(subscriber_cmd)
+    
     return ld
