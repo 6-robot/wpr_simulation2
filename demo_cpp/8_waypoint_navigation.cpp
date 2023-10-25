baabc5851c218aeada152d69d13f9341e177d63e
Robot <zhangwanjie@126.com>
2023-10-25 12:01:13 +0800

    update demo_8_waypoint

--- demo_cpp/8_waypoint_navigation.cpp
+++ demo_cpp/8_waypoint_navigation.cpp
@@ -1,22 +1,34 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
 
+std::shared_ptr<rclcpp::Node> node;
+
+void ResultCallback(const std_msgs::msg::String::SharedPtr msg)
+{
+    if(msg->data == "done")
+    {
+        RCLCPP_INFO(node->get_logger(), "Arrived !");
+    }
+}
 int main(int argc, char** argv)
 {
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<rclcpp::Node>("waypoint_navigation_node");
+    rclcpp::init(argc, argv);
+    node = std::make_shared<rclcpp::Node>("waypoint_navigation_node");
 
-  auto navigation_pub = node->create_publisher<std_msgs::msg::String>("/waterplus/navi_waypoint", 10);
+    auto navigation_pub = node->create_publisher<std_msgs::msg::String>(
+    "/waterplus/navi_waypoint", 10);
 
-  rclcpp::sleep_for(std::chrono::milliseconds(1000));
+    auto lidar_sub = node->create_subscription<std_msgs::msg::String>(
+    "/waterplus/navi_result", 10, ResultCallback);
 
-  std_msgs::msg::String waypoint_msg;
-  waypoint_msg.data = "1";
+    rclcpp::sleep_for(std::chrono::milliseconds(1000));
 
-  navigation_pub->publish(waypoint_msg);
+    std_msgs::msg::String waypoint_msg;
+    waypoint_msg.data = "1";
+    navigation_pub->publish(waypoint_msg);
 
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-
-  return 0;
+    rclcpp::spin(node);
+    
+    rclcpp::shutdown();
+    return 0;
 }
