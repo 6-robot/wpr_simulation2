183626318a6f8a05b9c8a8e5055a6981037d2ff6
Robot <zhangwanjie@126.com>
2023-10-27 22:13:02 +0800

    cv_follow

--- demo_cpp/9_cv_follow.cpp
+++ demo_cpp/9_cv_follow.cpp
@@ -1,8 +1,8 @@
 #include <rclcpp/rclcpp.hpp>
-#include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/msg/image.hpp>
+#include <cv_bridge/cv_bridge.h>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui/highgui.hpp> 
 #include <geometry_msgs/msg/twist.hpp>
 
 std::shared_ptr<rclcpp::Node> node;
@@ -28,45 +28,45 @@ void Cam_RGB_Callback(const sensor_msgs::msg::Image::SharedPtr msg)
     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
 
     Mat imgOriginal = cv_ptr->image;
-
-    // Convert the RGB image to HSV
+    
+    // Convert RGB image to HSV
     Mat imgHSV;
-    vector<Mat> hsvSplit;
     cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);
 
     // Perform histogram equalization in the HSV space
+    vector<Mat> hsvSplit;
     split(imgHSV, hsvSplit);
-    equalizeHist(hsvSplit[2],hsvSplit[2]);
-    merge(hsvSplit,imgHSV);
+    equalizeHist(hsvSplit[2], hsvSplit[2]);
+    merge(hsvSplit, imgHSV);
+
+    // Threshold the image using the specified Hue, Saturation, and Value thresholds
     Mat imgThresholded;
+    inRange(imgHSV, 
+        Scalar(iLowH, iLowS, iLowV), 
+        Scalar(iHighH, iHighS, iHighV), 
+        imgThresholded); 
 
-    // Use the above Hue, Saturation, and Value threshold ranges to binarize the image
-    inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); 
-
-    // Perform opening operation (remove some noise)
     Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
+    // Opening operation (remove some noise)
     morphologyEx(imgThresholded, imgThresholded, MORPH_OPEN, element);
-
-    // Perform closing operation (connect some connected domains)
+    // Closing operation (connect some regions)
     morphologyEx(imgThresholded, imgThresholded, MORPH_CLOSE, element);
 
-    // Iterate through the binary image data
+    // Traverse the binary image
     int nTargetX = 0;
     int nTargetY = 0;
     int nPixCount = 0;
     int nImgWidth = imgThresholded.cols;
     int nImgHeight = imgThresholded.rows;
-    int nImgChannels = imgThresholded.channels();
-    printf("Width = %d   Height= %d \n",nImgWidth,nImgHeight);
     for (int y = 0; y < nImgHeight; y++)
     {
-        for(int x = 0; x < nImgWidth; x++)
+        for (int x = 0; x < nImgWidth; x++)
         {
-            if(imgThresholded.data[y*nImgWidth + x] == 255)
+            if (imgThresholded.data[y * nImgWidth + x] == 255)
             {
                 nTargetX += x;
                 nTargetY += y;
-                nPixCount ++;
+                nPixCount++;
             }
         }
     }
@@ -74,7 +74,7 @@ void Cam_RGB_Callback(const sensor_msgs::msg::Image::SharedPtr msg)
     {
         nTargetX /= nPixCount;
         nTargetY /= nPixCount;
-        printf("Color centroid coordinates ( %d , %d )  Number of points = %d\n",nTargetX,nTargetY,nPixCount);
+        printf("Target (%d, %d) PixelCount = %d\n", nTargetX, nTargetY, nPixCount);
         // Draw coordinates
         Point line_begin = Point(nTargetX-10,nTargetY);
         Point line_end = Point(nTargetX+10,nTargetY);
@@ -83,8 +83,8 @@ void Cam_RGB_Callback(const sensor_msgs::msg::Image::SharedPtr msg)
         line_end.x = nTargetX; line_end.y = nTargetY+10; 
         line(imgOriginal,line_begin,line_end,Scalar(255,0,0),3);
         // Calculate robot motion speed
-        float fVelFoward = (nImgHeight/2-nTargetY)*0.002;     // Difference * Proportion
-        float fVelTurn = (nImgWidth/2-nTargetX)*0.003;           // Difference * Proportion
+        float fVelFoward = (nImgHeight/2-nTargetY)*0.002; // Difference * Proportion
+        float fVelTurn = (nImgWidth/2-nTargetX)*0.003;    // Difference * Proportion
         vel_cmd.linear.x = fVelFoward;
         vel_cmd.linear.y = 0;
         vel_cmd.linear.z = 0;
@@ -94,7 +94,7 @@ void Cam_RGB_Callback(const sensor_msgs::msg::Image::SharedPtr msg)
     }
     else
     {
-        printf("Target color disappeared...\n");
+        printf("Target disappeared...\n");
         vel_cmd.linear.x = 0;
         vel_cmd.linear.y = 0;
         vel_cmd.linear.z = 0;
@@ -107,7 +107,7 @@ void Cam_RGB_Callback(const sensor_msgs::msg::Image::SharedPtr msg)
     vel_pub->publish(vel_cmd);
 
     // Show the image
-    imshow("Threshold", imgThresholded);
+    imshow("Result", imgThresholded);
     imshow("Original", imgOriginal);
     cv::waitKey(5);
 }
@@ -118,9 +118,13 @@ int main(int argc, char** argv)
     node = std::make_shared<rclcpp::Node>("cv_follow_node");
 
     vel_pub = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-    auto sub = node->create_subscription<sensor_msgs::msg::Image>("/kinect2/qhd/image_raw", 10, Cam_RGB_Callback);
+    auto sub = node->create_subscription<sensor_msgs::msg::Image>(
+        "/kinect2/qhd/image_raw", 
+        10, 
+        Cam_RGB_Callback);
 
     rclcpp::spin(node);
+
     cv::destroyAllWindows();
     rclcpp::shutdown();
 
