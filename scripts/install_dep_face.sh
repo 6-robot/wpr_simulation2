39adfa8a9d10324e78a7307ab45754401d1beaff
Robot <zhangwanjie@126.com>
2023-11-21 08:28:26 +0800

    update scripts

---
+++ scripts/install_dep_face.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yes | pip3 install face_recognition
--- scripts/install_for_humble.sh
+++ scripts/install_for_humble.sh
@@ -8,13 +8,16 @@ cp -r ../../wpr_simulation2/worlds ~/.gazebo/models/wpr_simulation2/worlds
 sudo apt update -y
 sudo apt install -y ros-humble-gazebo-ros-pkgs
 sudo apt install -y ros-humble-gazebo-plugins
-sudo apt install -y ros-humble-ros2-control
-sudo apt install -y ros-humble-ros2-controllers
-sudo apt install -y ros-humble-gazebo-ros2-control
 sudo apt install -y ros-humble-slam-toolbox
 sudo apt install -y ros-humble-teleop-twist-keyboard
 sudo apt install -y ros-humble-navigation2
 sudo apt install -y ros-humble-nav2-bringup
+sudo apt install -y ros-humble-ros2-control
+sudo apt install -y ros-humble-ros2-controllers
+sudo apt install -y ros-humble-gazebo-ros2-control
 sudo apt install -y ros-humble-pcl-ros
+sudo apt install -y ros-humble-xacro
+sudo apt install -y ros-dev-tools
 sudo apt install -y python3-pip
-yes | pip3 install face_recognition
+sudo apt install -y python3-colcon-common-extensions
+sudo apt install -y python3-argcomplete
